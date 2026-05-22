91eac2fb2acb30a4c7b5d68918bdb68ad9054a6e
Mark <markvirgilvillarin@gmail.com>
2026-05-22 12:10:18 +0800

    update

--- backend/_helpers/send-email.ts
+++ backend/_helpers/send-email.ts
@@ -4,14 +4,15 @@ export default async function sendEmail({ to, subject, html, from }: any) {
   const emailFrom = from || process.env.EMAIL_FROM || 'info@node-mysql-api.com';
 
   const transporter = nodemailer.createTransport({
-    host: process.env.SMTP_HOST || 'smtp.ethereal.email',
-    port: parseInt(process.env.SMTP_PORT || '587', 10),
-    auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS
-    },
-    tls: { rejectUnauthorized: false }
-  } as any);
+  host: process.env.SMTP_HOST || 'smtp.ethereal.email',
+  port: parseInt(process.env.SMTP_PORT || '465', 10),
+  secure: true,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  },
+  tls: { rejectUnauthorized: false }
+} as any);
 
   const info = await transporter.sendMail({ from: emailFrom, to, subject, html });
   console.log('Email sent. Preview URL:', nodemailer.getTestMessageUrl(info));
